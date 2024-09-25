59381be86e1cadcc13cbf0f0950a5ea966e8f851
Cristhian Fernando Vasquez Albornoz <cristhianalbornoz9@gmail.com>
2024-09-25 02:18:05 -0500

    Inicio del Examen CL1 Back End

---
+++ src/main/java/com/example/demo/ExamenRueditasBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenRueditasBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenRueditasBackEndApplication.class, args);
+	}
+
+}
